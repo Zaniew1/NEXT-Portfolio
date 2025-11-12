7aa3c86c6b509f7e1c28fda1060b9f5c46f0013f
Mateusz <m.zaniewski1995@gmail.com>
2025-11-12 20:24:14 +0100

    chore: add downloadable cv

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -3,7 +3,8 @@ import styles from "./Nav.module.css";
 export const Nav = () =>{
     return(
         <nav className={styles.nav}>
-            <Link href="/" className={styles.nav__text}>Mateusz Zaniewski - Twój FullStack</Link>
+            <Link href="/" className={styles.nav__text}>Mateusz Zaniewski - FullStack Developer</Link>
+            <Link  download href="/CV1.pdf" className={styles.nav__cv}>Pobierz CV</Link>
         </nav>
     )
 
