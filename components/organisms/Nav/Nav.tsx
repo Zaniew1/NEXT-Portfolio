49cadd5d3021b323deab4c78788fd52dbade60e3
Mateusz <m.zaniewski1995@gmail.com>
2025-12-02 13:32:14 +0100

    fix: overlapping about text on project section

--- components/organisms/Nav/Nav.tsx
+++ components/organisms/Nav/Nav.tsx
@@ -4,7 +4,7 @@ export const Nav = () =>{
     return(
         <nav className={styles.nav}>
             <Link href="/" className={styles.nav__text}>Mateusz Zaniewski - FullStack Developer</Link>
-            <Link  download href="/CV1.pdf" className={styles.nav__cv}>Pobierz CV</Link>
+            <Link  download href="/Mateusz Zaniewski FullStack CV POL.pdf" className={styles.nav__cv}>Pobierz CV</Link>
         </nav>
     )
 
