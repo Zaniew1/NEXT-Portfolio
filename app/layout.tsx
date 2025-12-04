74b4860774a6e502fefcb885176acee172244404
Mateusz <m.zaniewski1995@gmail.com>
2025-12-04 18:23:06 +0100

    chore: add SpeedInsights to a project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Lexend, Roboto } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const roboto = Roboto({
   subsets: ["latin"],
@@ -35,6 +36,7 @@ export default function RootLayout({
         className={`${roboto.variable} ${lexend.variable} antialiased`}
       >
         {children}
+        <SpeedInsights/>
       </body>
     </html>
   );
