ec73ef9f819478083e7d980d61e228ef2377ac41
Mateusz <m.zaniewski1995@gmail.com>
2026-04-25 15:36:55 +0200

    chore: test vercel build

--- components/atoms/Language/Language.tsx
+++ components/atoms/Language/Language.tsx
@@ -1,6 +1,7 @@
 import styles from './Language.module.css'
 import Flag from 'react-world-flags'
 export const Language = (props: {setLang: (lang: "pl" | "en")=>void, lang:"pl" | "en"}) => {
+    console.log('asd')
     return (
             <div className={styles.lang} onClick={()=> props.setLang(props.lang === "pl" ? "en" :"pl")}>
                {props.lang === "pl" && <Flag alt="Flaga Polski" code={"pl"} /> }
