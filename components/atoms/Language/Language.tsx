64e5b269c6e071b56bdf0945eeb08d5ce8e98008
Mateusz <m.zaniewski1995@gmail.com>
2026-04-25 15:48:15 +0200

    chore: add contrast for wcag requirements

--- components/atoms/Language/Language.tsx
+++ components/atoms/Language/Language.tsx
@@ -1,7 +1,6 @@
 import styles from './Language.module.css'
 import Flag from 'react-world-flags'
 export const Language = (props: {setLang: (lang: "pl" | "en")=>void, lang:"pl" | "en"}) => {
-    console.log('asd')
     return (
             <div className={styles.lang} onClick={()=> props.setLang(props.lang === "pl" ? "en" :"pl")}>
                {props.lang === "pl" && <Flag alt="Flaga Polski" code={"pl"} /> }
